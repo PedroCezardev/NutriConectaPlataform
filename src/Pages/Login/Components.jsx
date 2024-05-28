f9b650224051ca2577249fc487219923110e2806
JhonB-DEVLP <jhonesbonifacio_18@outlook.com>
2024-05-28 19:31:04 -0300

    Organizando logout

--- src/Pages/Login/Components.jsx
+++ src/Pages/Login/Components.jsx
@@ -6,9 +6,9 @@ border-radius: 10px;
 box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 position: relative;
 overflow: hidden;
-width: 678px;
+width: 50%;
 max-width: 100%;
-min-height: 400px;
+height: 50%;
 `;
 
 export const SignUpContainer = styled.div`
